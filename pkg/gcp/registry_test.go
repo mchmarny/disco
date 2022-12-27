251ea31eb7ab952bd8652339b92c769b8664ff74
Mark Chmarny <mark@chmarny.com>
2022-12-26 19:09:58 -0800

    adds registry test

---
+++ pkg/gcp/registry_test.go
@@ -0,0 +1,15 @@
+package gcp
+
+import (
+	"context"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestRegistry(t *testing.T) {
+	client = &testAPIClient{}
+	info, err := GetImageInfo(context.Background(), "us-west1-docker.pkg.dev/cloudy-demos/artomator/artomator@sha256:b4a094e55244bc442bdaf2a5cd06a589f754ffc8ce09183868acaa79419cd88d")
+	assert.NoError(t, err)
+	assert.NotNil(t, info)
+}
