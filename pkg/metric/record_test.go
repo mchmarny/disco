5f4147bc7427dc75331e963cd1b9200a87cc6a83
Mark Chmarny <mark@chmarny.com>
2023-01-08 08:06:25 -0800

    fixes tag action

--- pkg/metric/record_test.go
+++ pkg/metric/record_test.go
@@ -1,10 +1,19 @@
 package metric
 
 import (
+	"context"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
+
+func TestAPICounter(t *testing.T) {
+	ctx := context.TODO()
+	c := &APICounter{}
+	records := []*Record{}
+	err := c.CountAll(ctx, records...)
+	assert.NoError(t, err)
+}
 
 func TestMetricType(t *testing.T) {
 	v := "vulnerability/high"
@@ -14,4 +23,30 @@ func TestMetricType(t *testing.T) {
 	v = "one-two-three"
 	c = MakeMetricType(v)
 	assert.Equal(t, "custom.googleapis.com/one-two-three", c)
+}
+
+func TestMetricRecord(t *testing.T) {
+	ctx := context.TODO()
+	c := &ConsoleCounter{}
+	err := c.Count(ctx, "test", 1, nil)
+	assert.NoError(t, err)
+	err = c.Count(ctx, "test", 1, map[string]string{"foo": "bar"})
+	assert.NoError(t, err)
+
+	records := []*Record{
+		{
+			MetricType:  "test1",
+			Labels:      map[string]string{"foo": "bar"},
+			MetricValue: 1,
+		},
+		{
+			MetricType:  "test2",
+			Labels:      map[string]string{"foo": "bar"},
+			MetricValue: 2,
+		},
+	}
+
+	err = c.CountAll(ctx, records...)
+	assert.NoError(t, err)
+
 }
